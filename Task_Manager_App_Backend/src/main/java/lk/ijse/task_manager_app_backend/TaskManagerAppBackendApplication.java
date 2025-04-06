50197f13c2002111a8045bd6c73613f259554b83
PasinduMshan <madushan01722@gmail.com>
2025-04-06 17:43:08 +0530

    Start create backend module

---
+++ Task_Manager_App_Backend/src/main/java/lk/ijse/task_manager_app_backend/TaskManagerAppBackendApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.task_manager_app_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagerAppBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskManagerAppBackendApplication.class, args);
+    }
+
+}
---
+++ Task_Manager_App_Backend/src/test/java/lk/ijse/task_manager_app_backend/TaskManagerAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.task_manager_app_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerAppBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
