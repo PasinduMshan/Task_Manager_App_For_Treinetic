4df3d10d9308f30a5a06f5628e39cc3149b33d69
PasinduMshan <madushan01722@gmail.com>
2025-04-07 08:55:10 +0530

    add model mapper dependency and add model mapper to project like bean

--- Task_Manager_App_Backend/src/main/java/lk/ijse/task_manager_app_backend/TaskManagerAppBackendApplication.java
+++ Task_Manager_App_Backend/src/main/java/lk/ijse/task_manager_app_backend/TaskManagerAppBackendApplication.java
@@ -1,7 +1,9 @@
 package lk.ijse.task_manager_app_backend;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TaskManagerAppBackendApplication {
@@ -10,4 +12,6 @@ public class TaskManagerAppBackendApplication {
         SpringApplication.run(TaskManagerAppBackendApplication.class, args);
     }
 
+    @Bean
+    public ModelMapper modelMapper() { return new ModelMapper(); }
 }
